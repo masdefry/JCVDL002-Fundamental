5ec89486ab12b1f016e0f40d3841e3bfb6a2b024
masdefry <muhammaddefryan@gmail.com>
2021-08-30 13:11:06 +0700

    Day3

---
+++ Day3-ReviewStudentsQuestion.js
@@ -0,0 +1,55 @@
+// function sumOfPositive(number1, number2){
+//     console.log(number1 + number2)
+// }
+
+// let angka1 = 10
+// let angka2 = 50
+
+// // sumOfPositive(angka1, angka2)
+
+// // 
+
+// var fullname = () => { // Ryan Defryan
+//     let firstName = "Ryan"
+//     let lastName = "Defryan"
+
+//     return firstName+lastName
+// }
+
+// // var result = fullname()
+
+// // console.log(result)
+
+// // 
+
+// // Saya ingin menampilkan angka dimulai dari 1-10
+
+// let angka = 1
+// while(angka <= 10){
+//     console.log(angka)
+//     angka++
+// }
+
+// for(let i=0; i<10; i++){
+//     console.log(i+1)
+// }
+
+//
+let point = 10 // 11
+
+function a(){
+    point++
+    return point
+    b()
+}
+
+function b(){
+    point += 10
+}
+
+function c(){
+    poin+10
+    a()
+}
+
+c()
--- Index.js
+++ Index.js
@@ -1,1 +1,24 @@
 // H E L L O   W O R L D !
+
+let arr = ["A", "B", "C"]
+arr[10] = "Purwadhika"
+
+console.log(arr.length)
+
+// 
+
+let point = "100"
+
+point += "3" // 13, 300
+point++
+point--
+point++ // 14, 301
+
+point += `1` 
+
+console.log(point)
+
+// 
+let arr1 = [1, 2, 3]
+let arr2 = [...arr1]
+console.log(arr2)
